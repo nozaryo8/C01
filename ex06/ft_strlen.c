5f694aa01444470fb0332924ed7c5ae747ef83c8
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-08-13 15:14:34 +0900

    fix ex06

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	int i ;
+	int i;
 	int count;
 
 	i = 0;
 	count = 0;
-
 	while(str[i] != '\0')
 	{
 		i++;
