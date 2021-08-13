be27f3f2c180caa9e9845bbe4724215800807c97
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-08-13 15:12:24 +0900

    fix ex04

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
-void ft_ultimate_div_mod(int *a, int *b){
-	int quotient;
-	int remainder;
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	quotient;
+	int	remainder;
 
 	quotient = *a / *b;
 	remainder = *a % *b;
-
 	*a = quotient;
 	*b = remainder;
 }
