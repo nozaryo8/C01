d63ace7a9ce1ae9fa59d66afdafa1ef2174cf97e
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-08-13 15:31:41 +0900

    fix ex08

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -9,8 +9,8 @@ void	ft_sort_int_tab(int *tab, int size)
 
 	i = 0;
 	j = 1;
-	while ( i < size ) {
-		while ( j < size ) {
+	while (i < size) {
+		while (j < size) {
 			if (tab[i] > tab[j]) {
 				tmp =  tab[i];
 				tab[i] = tab[j];
