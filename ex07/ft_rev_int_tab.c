76a45b787b836433a8c33d8a65d11b19bda7c27f
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-08-13 15:24:42 +0900

    fix ex07

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -1,19 +1,19 @@
-void ft_rev_int_tab(int *tab, int size)
+void	ft_rev_int_tab(int *tab, int size)
 {
-	int i;
-	int j;
-	int array[size];
+	int	i;
+	int	j;
+	int	array[size];
 
 	i = 0;
 	j = size - 1;
-	while(i < size)
+	while (i < size)
 	{
 		array[i] = tab[j];
 		i++;
 		j--;
 	}
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
 		tab[i] = array[i];
 		i++;
